eb089777804b0fb0a84c82797568673b7978e393
Chris <chris79gp@outlook.com>
2025-03-22 19:16:58 +0000

    Feat: update `Hero` component to be reusable among pages

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,64 +1,20 @@
-// import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import "./Hero.scss";
-import Video from "../Video/Video";
 
-import thurtonPromo from "/src/assets/videos/thurton-preschool-promo.mp4";
-
-// const API_URL = `${import.meta.env.VITE_CMS_URL}/api/title`;
-// const API_TOKEN = import.meta.env.VITE_CMS_API_TOKEN;
-
-const Hero = function () {
-	// const [homepage, setHomepage] = useState({});
-
-	// useEffect(() => {
-	// 	const fetchData = async () => {
-	// 		try {
-	// 			const response = await fetch(API_URL, {
-	// 				method: "GET",
-	// 				headers: {
-	// 					Authorization: `Bearer ${API_TOKEN}`,
-	// 					"Content-Type": "application/json",
-	// 				},
-	// 			});
-	// 			const data = await response.json();
-	// 			setHomepage(data.data);
-	// 		} catch (error) {
-	// 			console.error("Error fetching data:", error);
-	// 		}
-	// 	};
-
-	// 	fetchData();
-	// }, []);
-
+const Hero = function ({ heading, coverImg }) {
 	return (
 		<section className="hero">
-			<Video src={thurtonPromo} />
+			<img src={coverImg} alt="" className="hero__cover-img" />
 			<div className="hero__content fb-col-wrapper">
-				{/* <h1 className="hero__heading">{homepage.Title}</h1>
-				<span className="hero__subheading">{homepage.Description}</span>
-				<button className="hero__cta">{homepage.CallToAction}</button> */}
-				<h1 className="hero__heading">Thurton & Ashby St Mary Preschool</h1>
-				<div className="hero__body">
-					<p className="hero__subheading">
-						A unique, child-led outdoor setting based out of Thurton Village Hall and the
-						surrounding woodland, for children aged 2-5.
-					</p>
-					<p className="hero__subheading">
-						We offer fully funded hours with no additional charges, as well as fee paying places.
-					</p>
-				</div>
-				<div className="hero__actions">
-					<Link to="/contact" className="hero__cta">
-						Contact us
-					</Link>
-					<Link to="/" className="hero__cta">
-						Term dates
-					</Link>
-				</div>
+				<h1 className="hero__heading">{heading}</h1>
 			</div>
 		</section>
 	);
+};
+
+Hero.propTypes = {
+	heading: PropTypes.string.isRequired,
+	coverImg: PropTypes.string,
 };
 
 export default Hero;
