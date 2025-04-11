386098dbbfca6d032fa16e8122e8eeee3bd2c933
Chris <chris79gp@outlook.com>
2025-04-11 22:19:00 +0100

    Feat: add API call for `About` content and update styling for "About" page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
+import { Link } from "react-router-dom";
 import Hero from "../../partials/Hero/Hero";
 import TeamMembers from "./TeamMembers/TeamMembers";
 import Committee from "./Committee/Committee";
@@ -5,35 +8,52 @@ import "./About.scss";
 
 import coverImg from "/src/assets/images/about-us.jpg";
 
+const API_URL = `${import.meta.env.VITE_CMS_URL}/api/about`;
+const API_TOKEN = import.meta.env.VITE_CMS_API_TOKEN;
+
 const About = function () {
+	const [about, setAbout] = useState({
+		heading: "",
+		body: [],
+	});
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(API_URL, {
+					method: "GET",
+					headers: {
+						Authorization: `Bearer ${API_TOKEN}`,
+						"Content-Type": "application/json",
+					},
+				});
+				const data = await response.json();
+				setAbout(data.data);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		};
+
+		fetchData();
+	}, []);
+
 	return (
 		<>
 			<Hero heading="About Us" coverImg={coverImg} />
 			<section className="about fb-col-wrapper">
-				<h2>Welcome to Thurton Ashby & St Mary Preschool!!!</h2>
-				<p>
-					We provide a safe and nurturing environment in which children from 2 to 5 years of age
-					learn and develop through purposeful play. The children are at the centre of what we do
-					and we consistently reflect on the quality of our provision. We work with parents/carers
-					and other professionals ensuring all children reach their full potential, including
-					children with special educational needs and disabilities, and help them prepare for their
-					transition to school.
-				</p>
-				<p>
-					We offer the Early Years Foundation Stage (EYFS) curriculum allowing children to choose
-					from a range of play based activities and experiences, some of which are adult led and
-					some child initiated. These activities could be indoor or outdoor, which help them to gain
-					new skills, learn to work and co-operate with each other, understand risk and how to stay
-					safe, and explore the natural world. We encourage children to be outside as much as
-					possible and have an outdoor playroom and sheltered mud kitchen in a well resourced
-					garden. We also have access to a woodland area on the Village Hall&apos;s land to further
-					support the children&apos;s wonder of the natural world.
-				</p>
-				<p>
-					We also have access to a woodland area on the Village Hall&apos;s land to further support
-					the children&apos;s wonder of the natural world. We use this area to offer fun activities
-					based on exploring, discovering, using our imagination and watching the seasons change.
-				</p>
+				<h2 className="about__heading">{about.heading}</h2>
+				<BlocksRenderer
+					content={about.body}
+					blocks={{
+						paragraph: ({ children }) => <p className="about__paragraph">{children}</p>,
+						heading: ({ children }) => <h3 className="about__subheading">{children} </h3>,
+						link: ({ children, url }) => (
+							<Link to={url} target="_blank" className="about__link">
+								{children} (opens in new tab)
+							</Link>
+						),
+					}}
+				/>
 			</section>
 			<TeamMembers />
 			<Committee />
