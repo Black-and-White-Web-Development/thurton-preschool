b6207e026fce441c9f0838430fc1bc9f55a4833b
Chris <chris79gp@outlook.com>
2025-04-14 18:27:49 +0100

    Refactor: abstract `ContentBlock` component from `About.jsx`

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,12 @@
 import { useEffect, useState } from "react";
-import { BlocksRenderer } from "@strapi/blocks-react-renderer";
-import { Link } from "react-router-dom";
 import Hero from "../../partials/Hero/Hero";
+import ContentBlock from "../../partials/ContentBlock/ContentBlock";
 import TeamMembers from "./TeamMembers/TeamMembers";
 import Committee from "./Committee/Committee";
 import "./About.scss";
 
 import coverImg from "/src/assets/images/about-us.jpg";
 
-const BASE_URL = `${import.meta.env.VITE_CMS_URL}`;
 const API_URL = `${import.meta.env.VITE_CMS_URL}/api/about?populate[contentBlocks][populate]=image`;
 const API_TOKEN = import.meta.env.VITE_CMS_API_TOKEN;
 
@@ -46,34 +44,7 @@ const About = function () {
 				<h2 className="about__heading">{about.heading}</h2>
 				<div className="about__content-blocks">
 					{about.contentBlocks.map(block => (
-						<article key={block.id} className="content-block">
-							{block.image && (
-								<img
-									src={BASE_URL + block.image.url}
-									alt={block.image.alternativeText}
-									className="content-block__image"
-								/>
-							)}
-							<div className="content-block__content">
-								<h3 className="contentblock__heading">{block.heading}</h3>
-								<BlocksRenderer
-									content={block.body}
-									blocks={{
-										paragraph: ({ children }) => (
-											<p className="content-block__paragraph">{children}</p>
-										),
-										heading: ({ children }) => (
-											<p className="content-block__paragraph">{children} </p>
-										),
-										link: ({ children, url }) => (
-											<Link to={url} target="_blank" className="content-block__link">
-												{children} (opens in new tab)
-											</Link>
-										),
-									}}
-								/>
-							</div>
-						</article>
+						<ContentBlock key={block.id} content={block} />
 					))}
 				</div>
 			</section>
---
+++ src/partials/ContentBlock/ContentBlock.jsx
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
+import { Link } from "react-router-dom";
+
+const API_URL = `${import.meta.env.VITE_CMS_URL}`;
+
+const ContentBlock = function ({ content }) {
+	return (
+		<article key={content.id} className="content-block">
+			{content.image && (
+				<img
+					src={API_URL + content.image.url}
+					alt={content.image.alternativeText}
+					className="content-block__image"
+				/>
+			)}
+			<div className="content-block__content">
+				<h3 className="contentblock__heading">{content.heading}</h3>
+				<BlocksRenderer
+					content={content.body}
+					blocks={{
+						paragraph: ({ children }) => <p className="content-block__paragraph">{children}</p>,
+						heading: ({ children }) => <p className="content-block__paragraph">{children} </p>,
+						link: ({ children, url }) => (
+							<Link to={url} target="_blank" className="content-block__link">
+								{children} (opens in new tab)
+							</Link>
+						),
+					}}
+				/>
+			</div>
+		</article>
+	);
+};
+
+ContentBlock.propTypes = {
+	content: PropTypes.shape({
+		image: PropTypes.object,
+		heading: PropTypes.string,
+		body: PropTypes.array,
+	}),
+};
+
+export default ContentBlock;
